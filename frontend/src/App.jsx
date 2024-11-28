207e2853a3dd6eb6341ef49cfd989dbdc51812c2
Bijay Tamang <bijay3990@gmail.com>
2024-11-28 12:59:38 +0545

    SignUp functionailty done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,21 @@
 import './App.css'
 import Home from './pages/home/home'
-// import Login from './pages/login/login'
-// import Signup from './pages/signup/signup'
+import Login from './pages/login/login'
+import Signup from './pages/signup/signup'
+import { Routes, Route } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
 
 
 function App() {
   return (
     <>
       <div className='p-4 h-screen items-center justify-center'>
-        <Home/>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+        </Routes>
+        <Toaster/>
       </div>
     </>
   )
