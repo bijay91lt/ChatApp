23b12b04b4258d297e3a9985b3e024099310a9a3
Bijay Tamang <bijay3990@gmail.com>
2024-12-02 12:58:44 +0545

    Socket io and Authusers

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,5 +1,5 @@
-import express from "express"
-import protectRoute  from "../middleware/protectRoute.js";
+import express from "express";
+import protectRoute from "../middleware/protectRoute.js";
 import { getUsersForSidebar } from "../controllers/user.controller.js";
 
 const router = express.Router();
--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,36 +1,39 @@
-import React from 'react'
-import Conversation from './Conversation'
+import useGetConversation from "../../hooks/useGetConversation";
+import { getRandomEmoji } from "../../utils/emojis";
+import Conversation from "./Conversation";
 
 const Conversations = () => {
-  return (
-    <div className='py-2 flex flex-col overflow-auto'>
-        <Conversation/>
-        <Conversation/>
-        <Conversation/>
-        <Conversation/>
-        <Conversation/>
-    </div>
-  )
-}
+	const { loading, conversations } = useGetConversation();
+	return (
+		<div className='py-2 flex flex-col overflow-auto'>
+			{conversations.map((conversation, idx) => (
+				<Conversation
+					key={conversation._id}
+					conversation={conversation}
+					emoji={getRandomEmoji()}
+					lastIdx={idx === conversations.length - 1}
+				/>
+			))}
 
-export default Conversations
+			{loading ? <span className='loading loading-spinner mx-auto'></span> : null}
+		</div>
+	);
+};
+export default Conversations;
 
-
-
-//starter code
-// import React from 'react'
-// import Conversation from './Conversation'
+// STARTER CODE SNIPPET
+// import Conversation from "./Conversation";
 
 // const Conversations = () => {
-//   return (
-//     <div className='py-2 flex flex-col overflow-auto'>
-//         <Conversation/>
-//         <Conversation/>
-//         <Conversation/>
-//         <Conversation/>
-//         <Conversation/>
-//     </div>
-//   )
-// }
-
-// export default Conversations
+// 	return (
+// 		<div className='py-2 flex flex-col overflow-auto'>
+// 			<Conversation />
+// 			<Conversation />
+// 			<Conversation />
+// 			<Conversation />
+// 			<Conversation />
+// 			<Conversation />
+// 		</div>
+// 	);
+// };
+// export default Conversations;
